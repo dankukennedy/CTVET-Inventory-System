5c1caf539b36f73208427d6c002cfe187d84d91b
dankukennedy <dankukennedy@email.com>
2024-02-23 15:35:02 +0000

    new update

--- ctvet-inventory-management-system/client/src/Components/Login/Login.jsx
+++ ctvet-inventory-management-system/client/src/Components/Login/Login.jsx
@@ -1,51 +1,50 @@
- import React  from "react";
- import './Login.css'
- import {Link} from 'react-router-dom'
+import React from 'react'
+import './Login.css'
+import '../../App.css'
+import { Link } from 'react-router-dom'
 
- //importing our assets
+//import our Assets
 import video from '../../LoginAssets/video.mp4'
 import logo from '../../LoginAssets/logo.jpg'
 
- const Login = () =>{
-      return(
+const Login = () => {
+    return (
         <div className='loginPage flex'>
-           <div className='container flex'>
+        <div className="container flex">
 
             <div className="videoDiv">
                 <video src={video} autoPlay muted loop></video>
 
-               <div className="textDiv">
-                <h2 className='title'>create And Sell Extraordinary Product</h2>
-                <p>Adopt the peace of nature</p>
-               </div>
-                          
-                <div className="formDiv flex">
-                    <div className="headerDav">
-                        <img src={logo} alt = "Logo Image"/>
-                        <h3>Welcome Back!</h3>
-                    </div>
-                    <form action="" className="form-grid">
-                        <span>Login Status will go here</span>
-                        <div className="inputDav">
-                            <label htmlFor="username">Username</label>
-                            <div className="imput flex">
-                               <FaUserShield className='icon'/>
-                            </div>
-                        </div>
-                    </form>
+                <div className="textDiv">
+                   <h2 className='title'>Create Ad Extraordinary Products </h2>
+                   <p>Adopt the peace of nature!</p>
                 </div>
+
                 <div className="footerDiv flex">
-                    <span className="text">Don't have an account? </span>
-                    <Link to={'/register'}>
-                    <button className='btn'>Sign Up</button>
-                    </Link>
+                 <span className="text">Don't Have an account?</span>
+                 <Link to= {'/register'}>
+                 <button className='btn'>Sign Up</button>
+                 </Link>
                 </div>
             </div>
+ 
+             <div className="formDiv flex">
+                <div className="headerDiv">
+                    <img src={logo} alt =" Logo Image"/>
+                    <h3>Welcom Back!</h3>
+                </div>
+                 
+                <form action="" className='form grid'> 
+                   <span>Login Status will go here</span>
+                   <div className="inputDiv">
+                    
+                   </div>
+                </form>
+             </div>
 
-           </div>
         </div>
+        </div>
+    )
+}
 
-      )
- }
-
- export default Login
+export default Login
